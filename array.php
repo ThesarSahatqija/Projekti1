a4e7db46f58caec7fd2276686de1bdf37bc3003c
Thesar Sahatqija <thesar_@hotmail.com>
2021-02-14 00:40:44 +0100

    news

---
+++ array.php
@@ -0,0 +1,8 @@
+<?php 
+$lajmi1=array("Titulli"=>"Machiato","Pershkrimi"=>"Kafe e bere me qumesht");
+$lajmi2=array("Titulli"=>"Espresso","Pershkrimi"=>"Vetem kafe");
+
+$lajmi3=array("Titulli"=>"Capucino","Pershkrimi"=>"Permban kafe dhe qumesht");
+$lajmi4=array("Titulli"=>"Turke","Pershkrimi"=>"Vetem kafe dhe sheqer");
+$listaLajmeve= array($lajmi1, $lajmi2,$lajmi3,$lajmi4);
+?>
--- news.php
+++ news.php
@@ -1,14 +1,55 @@
+<?php 
+    include ("array.php");
+
+?>
 <?php
 
 include './components/session.php' ?>
 <!DOCTYPE html>
 <link rel="icon" href="img/logo.jpg" type="image/icon type">
-<html>
-    
+<html style="background-color: rgb(216, 214, 214);">
+  
 <head>
+<style>
+.lajmi {
+  border: 1px solid black;
+  width: 20%;
+  margin: 5px 1%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  border-radius: 5px;
+  background-color: cadetblue;
+}
+
+.main * {
+  font-family: sans-serif;
+}
+
+.main {
+  margin:100px;
+  width: 95vw;
+  height: 230px;
+  display: flex;
+  flex-direction: row;
+}</style>
     <link rel="stylesheet" href="style/style.css">
-<title>News</title>
+    
+<title>About us</title>
 <?php
 
 include './components/navbar.php' ?>
-</head>
+<div class="main">
+    <?php 
+        foreach($listaLajmeve as $lajmi){
+            echo "<div class='lajmi'>";
+            echo "<h1>".$lajmi["Titulli"]."</h1>";
+            echo "<p>".$lajmi["Pershkrimi"]."</p>";
+            echo "</div>";
+            if(true){}
+        }
+
+    ?>
+    
+</div>
